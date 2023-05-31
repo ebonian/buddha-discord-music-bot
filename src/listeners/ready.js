2f7d7ac6012c7663da64fe8507e2138bfd38eb2e
Ebonian <tanadon.santisan@gmail.com>
2023-05-31 14:00:28 +0700

    chore: update ready status

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,29 +1,24 @@
 module.exports = {
-  name: "ready",
-  run: async (client) => {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.user.setPresence({
-      status: "online",
-      activity: {
-        name: "-help",
-        type: "LISTENING",
-      },
-      // status: "dnd",
-      // activity: {
-      //   name: "Ebonian coding myself",
-      //   type: "WATCHING",
-      // },
-    });
+    name: "ready",
+    run: async (client) => {
+        console.log(`Logged in as ${client.user.tag}`);
+        client.user.setPresence({
+            status: "online",
+            activity: {
+                name: "Vishnu21",
+                type: "WATCHING",
+            },
+        });
 
-    if (client.spotify) await client.spotify.requestToken();
+        if (client.spotify) await client.spotify.requestToken();
 
-    const nodes = [...client.manager.nodes.values()];
-    for (const node of nodes) {
-      try {
-        await node.connect();
-      } catch (e) {
-        client.manager.emit("error", e, node);
-      }
-    }
-  },
+        const nodes = [...client.manager.nodes.values()];
+        for (const node of nodes) {
+            try {
+                await node.connect();
+            } catch (e) {
+                client.manager.emit("error", e, node);
+            }
+        }
+    },
 };
